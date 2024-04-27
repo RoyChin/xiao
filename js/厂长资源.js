0d4ec76a972548d5486ba0de010e2e3942e2e7c9
xiaoxiao2020 <147638837+xiaoxiao2020@users.noreply.github.com>
2024-04-28 01:11:34 +0800

    更新 厂长资源.js

--- js/厂长资源.js
+++ js/厂长资源.js
@@ -2,10 +2,9 @@
 // 地址发布页 https://cz01.vip
 var rule = {
     title: '厂长资源',
-    host:'https://www.czys.top',
-    // host:'https://www.czzy.site',
-    // host:'https://cz01.vip',
-    // hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});HOST = html.match(/推荐访问<a href="(.*)"/)[1];print("厂长跳转地址 =====> " + HOST)',
+    //host:'https://www.czys.top',
+     host:'https://cz01.vip',
+     hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});HOST = html.match(/推荐访问<a href="(.*)"/)[1];print("厂长跳转地址 =====> " + HOST)',
     url: '/fyclassfyfilter',
     filterable: 1,//是否启用分类筛选,
     filter_url: '{{fl.cateId}}{{fl.class}}{{fl.area}}/page/fypage',
