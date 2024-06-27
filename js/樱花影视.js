84a2b788bb074f3988d9861ad260c6a30c46b0a8
xiaoxiao2020 <147638837+xiaoxiao2020@users.noreply.github.com>
2024-06-28 01:23:13 +0800

    UP

---
+++ js/樱花影视.js
@@ -0,0 +1,37 @@
+var rule = {
+  title: '映画影视',
+  host: 'https://www.mayadc.com',
+  url: '/vodlist/fyclass_____addtime_fypage.html',
+  searchUrl: '/search/**-.html',
+  searchable: 2,
+  quickSearch: 0,
+  filterable: 0,
+  headers: {
+    'User-Agent': 'PC_UA',
+  },
+  class_parse: 'nav.nav li:gt(0):lt(4);a&&Text;a&&href;.*/(.*)/',
+  play_parse: true,
+  lazy: $js.toString(() => {
+        var url = JSON.parse(request(input).match(/_player = (.*?);</)[1]).url;
+        //console.log(url);
+        input = {
+                jx: 0,
+                url: url,
+                parse: 0
+            }
+
+    }),
+  limit: 6,
+  推荐: 'div.layout-box;li.w8;h3&&Text;.vod-image img&&src;.sname&&Text;a&&href',
+  double: true,
+  一级: '.video-list .w8;h3&&Text;a&&data-original;.sname&&Text;a&&href',
+  二级: {
+    title: 'h1&&Text;.vod-seat ul a:eq(0)&&Text',
+    img: '.lazyload&&data-original',
+    desc: '.txt-hid&&Text;.vod-info p:eq(8)&&Text;.vod-info p:eq(6)&&Text;;.vod-info p:eq(3)&&Text',
+    content: '.vod-info p:eq(1)&&Text',
+    tabs: 'ul#Tab.vod-player li',
+    lists: '.fade-in:eq(#id) li',
+  },
+  搜索: '.main-left;h3&&Text;.lazyload&&data-original;.txt-hidden&&Text;a&&href;.search-infos&&Text',
+}
