c9c079c5940f81d82b2979f1c30374ab395460c9
xiaoxiao2020 <147638837+xiaoxiao2020@users.noreply.github.com>
2024-08-07 19:15:49 +0800

    更新 HDmoli.js

--- js/HDmoli.js
+++ js/HDmoli.js
@@ -11,7 +11,15 @@ var rule = {
   },
   class_parse: '.myui-header__menu li;a&&Text;a&&href;index(\\d+)\.html',
   play_parse: true,
-  lazy: '',
+          lazy: $js.toString(() => {
+        let js = 'try{function requestApix(callback){$.post(\"ass.php\",{vid:getQueryString(\"vid\")},function(result){callback(result.data.url);},\"json\");}requestApix(function(data){location.href=sign(data);})}catch(e){}location.href=document.querySelector(\"#playleft iframe\").src;';
+        input = {
+            parse: 1,
+            url: input,
+            click: js,
+            js: js
+        };
+    }),
   limit: 6,
   推荐: 'ul.myui-vodlist.clearfix;li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
   double: true,
