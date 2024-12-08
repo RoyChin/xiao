7f4dbb76de2cd885792021e1459c2e72a5050b5d
xiaoxiao2020 <147638837+xiaoxiao2020@users.noreply.github.com>
2024-12-09 00:16:47 +0800

    UP

--- js/金金虫.js
+++ js/金金虫.js
@@ -3,4 +3,6 @@ var rule = {
   模板: '自动',
   host: 'https://www.jinjinchong.com',
   url:'/vodshow/fyclass--------fypage---.html',//网站的分类页面链接
+  searchUrl:'/vodsearch/**----------fypage---.html',
+  cate_exclude:'福利',
 }
