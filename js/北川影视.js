cca06ba20afc1c21217d0d33ddd74b4894a87cf7
xiaoxiao2020 <8152552@qq.com>
2025-03-17 19:40:17 +0800

    Update 北川影视.js

--- js/北川影视.js
+++ js/北川影视.js
@@ -1,8 +1,9 @@
+//https://www.bcyingshi.ink/fb 发布页
 var rule = {
     模板: '自动',
     title: '北川影视',
-    host: 'https://www.bcyingshi.ink/fb',
-    hostJs: 'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(2)&&a&&href");print(src);HOST=src',
+    host: 'https://www.bcdy.xyz',
+    //hostJs: 'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(2)&&a&&href");print(src);HOST=src',
     searchUrl: '/vodsearch/page/fypage/wd/**/',
     tab_exclude:'君离牛逼|安娜动漫',
 }
