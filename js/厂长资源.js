fd8c63cf6b3da53db392c8535ecc750b4e357142
xiaoxiao2020 <8152552@qq.com>
2025-03-18 21:31:11 +0800

    Update 厂长资源.js

--- js/厂长资源.js
+++ js/厂长资源.js
@@ -8,8 +8,8 @@ var rule = {
     filterable: 1,//是否启用分类筛选,
     filter_url: '{{fl.cateId}}{{fl.class}}{{fl.area}}/page/fypage',
     filter: 'H4sIAAAAAAAAA42V3U7iQBTH36XXJt77KhtjBql0EFqlHbUYE79AQEXcVfwiarJi8QOVNborUniZTluufIXtiO1U7LRe9KLT3/+cOWfO/LvIpaU5yE/EFG7sxyI3zavcGAcyPOBGOBGkeecNF/JW69V5nwMpxL9jc84yAcjXXKO/1uCWRgaro264CZnPQF4ezQpAnHSehIJgEgLxQ2bdHPX3e2a5YW9X2OK4qrr83hPWWyGkE1oWYBJR3tbyuKiFSCQBAbeKUsPae2WzCSSRIrzw+EQ32lpo+HTS3Uq3HApmXNA8qIeCggv2z67CwXkv5NNffFF14LdOAeeejdfqW6cYIkRARaSVKhQTbqnbZfu+GdX/NA+dHg1JzVvNKT5KmoExXhxWHtTN2m2UkozVl6SkOSd6lJQIvmrtzdY3tFMgoNTH/e9ljX/t7wNuX0Yp+RSSZDgkNXrrln5gVu+j1OTigTgYzlw6Nzd7+EKL7DOCKgJi1hkPcda9AkbnGO8eGv9+4a02tzROxAP3mEwBWab24XgHPt38ZB8+3xgZeAlRD4Ke23cPH6t0HwpIyKOTJH0Seixe71k39WCWV3iFgvmf/aOrYDANpAXiSh5b1My1XDCbRLOkeR5aahh6LRiNS1kk+Umzc8Mi0/70Ax8KJgWBcuU83vkTzKWgY4SU3LmwfzMKSqD3I/WVVNkyl7VgWJHExMKn+qs14kKB8AJxYg+sb+AXRlGzkOyAonfPuN1ktB8CxZ/fXKmYy9VgdhrFaH5z9daq7jI2isiVoOhazi4y+koOX+Z9aMlcPWakJ6jk32vh0GgXg2Hyh8wKPG2C2dyyC6eMI5v3YZdG9zwYm0fOVHuktdLtH3cZ2QEUfQdgFVrM+Qfw0/xbpXtLvw5Gp5ySEG2rdda0ihuMsZ6hmLbLHJRpOvxW+xrr+8yAydQMDWnXe9Rmhs8+RrFexbl6wZga97D+2aPxUmFgKnHB8aX/CTefD1IJAAA=',
-    searchUrl: '/daoyongjiek0shibushiyoubing?q=**&f=_all&p=fypage',
-    
+    searchUrl: '/',
+
     searchable: 2,
     filterable: 0,
     headers: {
@@ -69,46 +69,35 @@ var rule = {
     },
     搜索: `js:
     let cookie = getItem(RULE_CK,'');
-    // let cookie = '';
-    log('储存的cookie:'+cookie);
-    let hhtml=request(input,{withHeaders:true,headers:{Cookie:cookie}});
-    let json = JSON.parse(hhtml);
-    let html = json.body;
-    let setCk = Object.keys(json).find(it=>it.toLowerCase()==='set-cookie');
-    cookie = setCk ? json[setCk] : cookie;
-    // 3个set-Cookie
-    if (Array.isArray(cookie)) {
-        cookie = cookie.join(';');
-    }
-    cookie = cookie.split(';')[0];
-    log('set-cookie:'+cookie);
-    let code='';
-    if(/erphp-search-captcha/.test(html)){
-        code = jsp.pdfh(html,'.erphp-search-captcha--button&&Text');
-        if(code.includes('=')){
-            let a = code.replace('=','').replace(/ /g,'');
-            code = eval(a);
-            log('回答验证码:'+a+' 答案:'+code);
-        }
-        let key = jsp.pdfh(html,'.erphp-search-captcha&&input&&name');
-        let body = key+'='+code;
-        post(input,{body:body,headers:{Cookie:cookie}});
-        setItem(RULE_CK,cookie);
-        html = getHtml(input);
-    }
-    // log(html);
-    VODS = [];
-    let lis=pdfa(html,'.search_list&&ul&&li');
-    log(lis.length);
-    lis.forEach(function(it){
-        VODS.push({
-            vod_id: pd(it,'a&&href',input),
-			vod_name: pdfh(it,'h3.dytit&&Text'),
-			vod_pic: pd(it,'img.lazy&&data-original',input),
-			vod_remarks: pdfh(html,'.jidi&&Text')
-        });
-    
+// let cookie = '';
+log('储存的cookie:'+cookie);
+pdfh = jsp.pdfh;
+let htmlsearch = request(input);
+let htmlurl = pdfh(htmlsearch, '.w-search-form&&action');
+
+let hhtml=request(htmlurl + "?q=" + KEY + "&f=_all&p=" + MY_PAGE,{withHeaders:true,headers:{Cookie:cookie}});
+let json = JSON.parse(hhtml);
+let html = json.body;
+let setCk = Object.keys(json).find(it=>it.toLowerCase()==='set-cookie');
+cookie = setCk ? json[setCk] : cookie;
+// 处理多个set-Cookie的情况
+if (Array.isArray(cookie)) {
+    cookie = cookie.join(';');
+}
+cookie = cookie.split(';')[0];
+log('set-cookie:'+cookie);
+
+VODS = [];
+let lis=pdfa(html,'.search_list&&ul&&li');
+log(lis.length);
+lis.forEach(function(it){
+    VODS.push({
+        vod_id: pd(it,'a&&href',htmlurl),
+        vod_name: pdfh(it,'h3.dytit&&Text'),
+        vod_pic: pd(it,'img&&src',htmlurl),
+        vod_remarks: pdfh(html,'.jidi&&Text')
     });
+});
     
     `,
 
