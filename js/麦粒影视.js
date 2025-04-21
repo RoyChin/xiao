2b30f3066fca4d9f7962aee1b64cee65d2ab1523
xiaoxiao2020 <8152552@qq.com>
2025-04-21 19:10:49 +0800

    Update 麦粒影视.js

--- js/麦粒影视.js
+++ js/麦粒影视.js
@@ -7,5 +7,5 @@ var rule = {
     class_parse: 'div.nav ul.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
     cate_exclude:'体育赛事|全部影片',
     一级: '.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a.module-item-title&&href',
-    搜索: '.module-items .module-search-item;a&&title;img&&data-src;.video-serial&&Text;a.video-serial&&href',
+    搜索: '.module-items .module-search-item;a.video-serial&&title;img&&data-src;.video-serial&&Text;a.video-serial&&href',
 }
